2424f4d47336dfd342821455e20cc87766590894
b-fang <shampuk@gmail.com>
2024-04-29 13:10:47 +0300

    add seminar_3 tasks

--- mydjangoproject/gamesapp/urls.py
+++ mydjangoproject/gamesapp/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('coin/', views.coinflip, name='coinflip'),
     path('number/', views.random_number, name='random_number'),
     path('last/', views.get_last_n_coins, name='last_coins'),
+    path('number/<int:count>/', views.get_n_numbers, name='get_n_numbers'),
+    path('cube/<int:count>/', views.get_n_cubes, name='get_n_cubes'),
+    path('coin/<int:count>/', views.get_n_coins, name='get_n_coins'),
 ]
--- mydjangoproject/gamesapp/views.py
+++ mydjangoproject/gamesapp/views.py
@@ -15,12 +15,33 @@ def coinflip(request):
     return HttpResponse(value.name)
 
 
+def get_n_coins(request, count):
+    context = {'title': 'рандомные монетки', 'content': []}
+    for i in range(count):
+        context['content'].append(random.choice(['Орел', 'Решка']))
+    return render(request, 'gamesapp/base_game.html', context)
+
+
 def cube(request):
     return HttpResponse(random.randint(1, 6))
 
 
+def get_n_cubes(request, count):
+    context = {'title': 'рандомные кубики', 'content': []}
+    for i in range(count):
+        context['content'].append(random.randint(1, 6))
+    return render(request, 'gamesapp/base_game.html', context)
+
+
 def random_number(request):
     return HttpResponse(random.randint(0, 100))
+
+
+def get_n_numbers(request, count):
+    context = {'title': 'рандомные числа', 'content': []}
+    for i in range(count):
+        context['content'].append(random.randint(1, 100))
+    return render(request, 'gamesapp/base_game.html', context)
 
 
 def get_last_n_coins(request):
