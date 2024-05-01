ea058feeca9b5f4dbaf1c01b6ebdfc9fa21a6cde
b-fang <shampuk@gmail.com>
2024-05-01 12:27:10 +0300

    add seminar_5 tasks

--- mydjangoproject/blogapp/admin.py
+++ mydjangoproject/blogapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Author
+
 
 # Register your models here.
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'surname', 'email', 'birthday')
+    list_filter = ('name',)
+    search_fields = ('name',)
+    fields = ('name', 'surname', 'email', 'bio', 'birthday')
+    readonly_fields = ('birthday',)
+
+
+admin.site.register(Author, AuthorAdmin)
