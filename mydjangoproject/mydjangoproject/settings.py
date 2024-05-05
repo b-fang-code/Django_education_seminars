3263c70c3ab68dd1ce277e0b4cd474ffb2e5df15
b-fang <shampuk@gmail.com>
2024-05-05 09:39:28 +0300

    add seminar_6 tasks

--- mydjangoproject/mydjangoproject/settings.py
+++ mydjangoproject/mydjangoproject/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'debug_toolbar',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,7 +45,12 @@ INSTALLED_APPS = [
     'blogapp',
 ]
 
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
+
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- mydjangoproject/mydjangoproject/urls.py
+++ mydjangoproject/mydjangoproject/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('__debug__/', include("debug_toolbar.urls")),
     path('form', include('gamesapp.urls')),
     path('', include('myapp.urls')),
     path('games/', include('gamesapp.urls')),
